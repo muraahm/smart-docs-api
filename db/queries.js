caf4837d7e2fc3903e5364300d790686a2bf71cf
Ahmed <sultan_902002@yahoo.com>
2019-11-02 16:13:58 -0600

    add queries for user registeration

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,22 @@
 module.exports = db => ({
 
+  getFiles() {
+    return db.query(
+      `SELECT * FROM files`
+    )
+      .then(({ rows: files }) => files)
+      .catch(error => console.log(error));
+  },
+
+  getUserByEmail(email) {
+    return db.query(
+      `SELECT * FROM users WHERE email = $1`, [
+      email]
+    )
+      .then(({ rows }) => rows[0])
+      .catch(error => console.log(error));
+  },
+
   getUsers() {
     return db.query(
       `SELECT * FROM users`
@@ -7,6 +24,7 @@ module.exports = db => ({
       .then(({ rows: users }) => users)
       .catch(error => console.log(error));
   },
+
 
   createUser(name, email, password) {
     return db.query(`
@@ -21,6 +39,18 @@ module.exports = db => ({
     ]
     )
       .then(({ rows: user }) => user)
+      .catch(error => console.log(error));
+  },
+
+  createUserFile(name, user_id) {
+    return db.query(`
+    INSERT INTO "files"
+    (name, user_id)
+    VALUES ($1, $2);
+    `, [
+      name, user_id
+    ]
+    )
       .catch(error => console.log(error));
   },
 
