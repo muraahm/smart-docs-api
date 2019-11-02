156fbfccfb4cc31fe7f3c0f329717e29c040b634
Ahmed <sultan_902002@yahoo.com>
2019-11-02 01:48:01 -0600

    add create new user query with hashed password and put user route

--- db/queries.js
+++ db/queries.js
@@ -6,6 +6,22 @@ module.exports = db => ({
     )
       .then(({ rows: users }) => users)
       .catch(error => console.log(error));
-  }
+  },
+
+  createUser(name, email, password) {
+    return db.query(`
+    INSERT INTO "users"
+    (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+    `, [
+      name,
+      email,
+      password
+    ]
+    )
+      .then(({ rows: user }) => user)
+      .catch(error => console.log(error));
+  },
 
 })
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = (query) => {
   router.get("/users", (req, res) => {
     query.getUsers()
       .then(users => res.json(users))
   });
-  
+
+  router.put("/users/new", (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    bcrypt.hash(req.body.password, saltRounds, function (err, hashedPassword) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        req.body.password = hashedPassword;
+        query.createUser(name, email, hashedPassword)
+          .then(user => res.json(user))
+          .catch(error => console.log(error));
+      }
+    })
+
+  });
+
   return router
 }
