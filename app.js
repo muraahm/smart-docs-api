852005b9f2a035af96a68906988d180b36d50e35
Ahmed <sultan_902002@yahoo.com>
2019-11-02 01:08:15 -0600

    add user route and refactor code to connect local db

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const PORT = process.env.PORT || 3001;
+const http = require('http');
+const db = require("./db");
+const app = express();
+server = http.createServer(app)
+const query = require("./db/queries")(db)
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +16,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const users = require("./routes/users");
 
+
+app.use("/api", users(query));
+
+
+server.listen(PORT, () => console.log("I'm listening on " + PORT))
 module.exports = app;
